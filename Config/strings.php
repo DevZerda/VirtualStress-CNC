4e84d027722a4dda25d9563adeb8e4320c29b0ed
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-01-27 14:03:10 -0500

    Update strings.php

--- Config/strings.php
+++ Config/strings.php
@@ -51,8 +51,31 @@ $CurrentUser = array(
     "Admin" => false
 );
 
-function GetUserInfo($usernameOrip) {
+$Colors = array(
+    "Red" => "",
+    "Yellow" => "\x1b[33m",
+    "Blue" => "\x1b[34m",
+    "Purple" => "\x1b[95m",
+    "Green" => "\x1b[32m",
+    "Cyan" => "\x1b[96m",
+    "Black" => "\x1b[30m",
+    "Grey" => "\x1b[90m",
+    "Reset" => "\x1b[39m",
+    "Background_Red" => "\x1b[41m",
+    "Background_Green" => "\x1b[42m",
+    "Background_Grey" => "\x1b[100m",
+    "Background_Reset" => "\x1b[49m",
+    "Clear" => "\033[2J\033[1;1H"
+)
 
+function GetUserInfo($info) {
+    $get_user = explode(",", $info);
+    $CurrentUser["Username"] = $info[0];
+    $CurrentUser["IP"] = $info[1];
+    $CurrentUser["Password"] = $info[2];
+    $CurrentUser["Level"] = $info[3];
+    $CurrentUser["Maxtime"] = $info[4];
+    $CurrentUser["Admin"] = $info[5];
 }
 
 ?>

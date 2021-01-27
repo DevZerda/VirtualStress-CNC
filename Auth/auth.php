adffc45306b2ba49b92fa2920c16f90629234bc1
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-01-27 17:24:21 -0500

    Update auth.php

--- Auth/auth.php
+++ Auth/auth.php
@@ -2,11 +2,15 @@
 
 class AUTH 
 {
-    public function login($usr, $pw, $db_usr, $db_pw,) {
-        if($usr == $db_usr || $pw == $db_pw) {
-            return "Successfully logged in, Welcome $usr\r\n"
+    public function login($usr, $pw, $db_usr, $db_pw) {
+        if(empty($usr) || empty($pw)) {
+            return "Error, Fill in the blank to continue\r\n";
         } else {
-            return "[x] Error, Username or password is incorrect!\r\n";
+            if($usr == $db_usr && $pw == $db_pw) {
+                return "Successfully logged in, Welcome $usr\r\n";
+            } else {
+                return "[x] Error, Username or password is incorrect!\r\n";
+            }
         }
     }
 }

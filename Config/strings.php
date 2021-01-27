acee2b4e33ddcddefbb7f30dc7dcd637bec18d81
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-01-27 16:01:08 -0500

    Update strings.php

--- Config/strings.php
+++ Config/strings.php
@@ -27,55 +27,56 @@ Maxtime             $CurrentUser["Maxtime"];
 Admin               $CurrentUser["Admin"];
 */
 
-$CNC_Info = array(
-    "Title" => "Virtual Stress CNC",
-    "Description" => "Bypassing All Connection!",
-    "Version" => "1.00",
-    "Creator" => "Scrapy",
-    "Owners" => array("Scrapy", "DirtyPorts"),
-    "Verifcation Code" => ""
-);
+class STRING {
+    public $CNC_Info = array(
+        "Title" => "Virtual Stress CNC",
+        "Description" => "Bypassing All Connection!",
+        "Version" => "1.00",
+        "Creator" => "Scrapy",
+        "Owners" => array("Scrapy", "DirtyPorts"),
+        "Verifcation Code" => ""
+    );
 
-$CurrentMSG = array(
-    "Cmd" => "",
-    "Fullcmd" => "",
-    "arg" => array("", "")
-);
+    public $CurrentMSG = array(
+        "Cmd" => "",
+        "Fullcmd" => "",
+        "arg" => array("", "")
+    );
 
-$CurrentUser = array( 
-    "Username" => "",
-    "IP" => "",
-    "Password" => "",
-    "Level" => "",
-    "Maxtime" => "",
-    "Admin" => false
-);
+    public $CurrentUser = array( 
+        "Username" => "",
+        "IP" => "",
+        "Password" => "",
+        "Level" => "",
+        "Maxtime" => "",
+        "Admin" => false
+    );
 
-$Colors = array(
-    "Red" => "",
-    "Yellow" => "\x1b[33m",
-    "Blue" => "\x1b[34m",
-    "Purple" => "\x1b[95m",
-    "Green" => "\x1b[32m",
-    "Cyan" => "\x1b[96m",
-    "Black" => "\x1b[30m",
-    "Grey" => "\x1b[90m",
-    "Reset" => "\x1b[39m",
-    "Background_Red" => "\x1b[41m",
-    "Background_Green" => "\x1b[42m",
-    "Background_Grey" => "\x1b[100m",
-    "Background_Reset" => "\x1b[49m",
-    "Clear" => "\033[2J\033[1;1H"
-);
+    public $Colors = array(
+        "Red" => "",
+        "Yellow" => "\x1b[33m",
+        "Blue" => "\x1b[34m",
+        "Purple" => "\x1b[95m",
+        "Green" => "\x1b[32m",
+        "Cyan" => "\x1b[96m",
+        "Black" => "\x1b[30m",
+        "Grey" => "\x1b[90m",
+        "Reset" => "\x1b[39m",
+        "Background_Red" => "\x1b[41m",
+        "Background_Green" => "\x1b[42m",
+        "Background_Grey" => "\x1b[100m",
+        "Background_Reset" => "\x1b[49m",
+        "Clear" => "\033[2J\033[1;1H"
+    );
 
-function GetUserInfo($info) {
-    $get_user = explode(",", $info);
-    $CurrentUser["Username"] = $info[0];
-    $CurrentUser["IP"] = $info[1];
-    $CurrentUser["Password"] = $info[2];
-    $CurrentUser["Level"] = $info[3];
-    $CurrentUser["Maxtime"] = $info[4];
-    $CurrentUser["Admin"] = $info[5];
+    public function GetUserInfo($info) {
+        $get_user = explode(",", $info);
+        $this->CurrentUser["Username"] = $get_user[0];
+        $this->CurrentUser["IP"] = $get_user[1];
+        $this->CurrentUser["Password"] = $get_user[2];
+        $this->CurrentUser["Level"] = $get_user[3];
+        $this->CurrentUser["Maxtime"] = $get_user[4];
+        $this->CurrentUser["Admin"] = $get_user[5];
+    }
 }
-
 ?>

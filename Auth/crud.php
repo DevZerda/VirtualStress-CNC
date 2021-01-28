3bba0bc49250650ae58f65863c95162ed42d6bb3
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-01-28 08:05:43 -0500

    Update crud.php

--- Auth/crud.php
+++ Auth/crud.php
@@ -1,10 +1,10 @@
 <?php
 
 /*
-            CRUD Doc
+                            CRUD Doc
 
-Get Username Info               USER(usernameOrip, stat_type);
-                                        Stat Types: 
+Get Username Info               USER(usernameOrip, stat_type);                                      Return Value Function
+                                        Stat Types:                    
                                                 - username
                                                 - ip
                                                 - password
@@ -13,15 +13,15 @@ Get Username Info               USER(usernameOrip, stat_type);
                                                 - admin
                                                 - all [(one string between commas)]
 
-Add User                        AddUser($user, $ip, $password, $level, $maxtime, $level);
+Add User                        AddUser($user, $ip, $password, $level, $maxtime, $level);           Return Value Function
 
-Remove User                     RemoveUser($user);
+Remove User                     RemoveUser($user);                                                  Return Value Function
 
-Check if Signed In              isSignIn(usernameOrip);
+Check if Signed In              isSignIn(usernameOrip);                                             Return Boolen Function
 
-Check If Premium                isPremium($usernameOrip);
+Check If Premium                isPremium($usernameOrip);                                           Return Boolen Function
 
-Check If Admin                  isAdmin($usernameOrip);
+Check If Admin                  isAdmin($usernameOrip);                                             Return Boolen Function
 */
 class CRUD 
 {
@@ -99,19 +99,69 @@ class CRUD
     }
 
     public function log_session($ip, $username) {
+        // Look into the CSV functions or read/write JSON. Much faster, more elegant.
+        // Less cancerware ;)
+        // Base ref for CSV: https://www.php.net/manual/de/function.fgetcsv.php
+        // For json: json_encode/json_decode to encode and decode JSON
+        // Keep this here. i'll check it out
+        /*
         $db = fopen("./db/current.db", "a");
         fwrite($db, "('$username','$ip')\n");
         fclose($db);
+        */
+        $db = file_get_contents("./db/current.db");
+        try {
+            $users = json_decode($db);
+        } catch(\Exception $e) {
+            // log the error from: $e->what()
+        }
+
+        if(!in_array($username, $users["listed"])) {
+            $users["listed"][$username] = $ip;
+        }
+        //NEVER THOUGHT OF USING THIS vv file_put_contents(). THIS ONLY APPENDS? no overwrite?
+        // docs: https://www.php.net/manual/en/function.file-put-contents.php
+        // you dutch ? lol
+        file_put_contents("./db/current.db", json_serialize($users));
     }
 
     public function remove_session($ip) {
         $old_db = file_get_contents("./db/current.db");
-        $old_users = explode("\n", $old_db);
+        try {
+            $old_users = json_decode($old_db);
+        } catch(\Exception $e) {
+            // Handle error
+        } // in ? or as? v lmao thats right v
+        $found_user = null;
+        foreach($old_db["listed"] as $user_name => $user_ip) {
+            if($user_ip == $ip) $found_user = $user_name;
+        }
+        //wym like show here?
+        if(!is_null($found_user)) {
+            delete($old_db["listed"][$found_user]);
+            file_put_contents("./db/current.db", json_serialize($old_db));
+        }
+    }
 
-        $new_db = "";
-        // foreach($old_users as $usrr) {
-            
-        // }
+    public function isSignedIn($status) {
+        //BRUH THIS IS WRONG. THIS SUPPOSE TO BE DOING SOMETHING ELSE LMAO
+        // then go fix it? o.o yes // read file from current.db to see if user is in it (HERE AN USER LINE: [ '('Jeff','1.1.1.1')' ]
+        // I'll just reimpl all three of that. Gimme a sec.
+    }
+
+    public function isPremium($usr) {
+        $get_user = $this->USER($usr, "all")[3];
+        if($get_user )
+        if(intval($status) == 0) {
+            return false;
+        } else if(intval($status) > 0 || intval($status) < 5) {
+            return true;
+        }
+    } // this my first source in OOP. wait its possible to use $this->USER();?
+    //idk why im fetching info from USER() here in main file to send back to these functions LMAO
+
+    public function isAdmin($status) {
+        
     }
 }
 ?>
